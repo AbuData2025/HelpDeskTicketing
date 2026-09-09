ab12d23a2e4bfa1086f234107fce71ec4f3e5608
AbuData2025 <Abulelemndini@gmail.com>
2026-09-10 01:26:44 +0200

    Customize registration: add FullName field, auto-assign EndUser role, scaffold Identity Register page

---
+++ Data/DbSeeder.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using HelpDeskTicketing.Models;
+
+namespace HelpDeskTicketing.Data
+{
+    public static class DbSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roles = { "Admin", "ITAgent", "EndUser" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,10 +11,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<HelpDeskTicketing.Models.ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await HelpDeskTicketing.Data.DbSeeder.SeedRolesAsync(scope.ServiceProvider);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
